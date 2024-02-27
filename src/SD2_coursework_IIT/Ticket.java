8d3591d0ed47f4bdf0d77d4f4dbeaa5cd7b62bde
nethsuka <nethsuka888@gmail.com>
2024-02-28 00:46:44 +0530

    update the person.java file and make a new ticket.java file

---
+++ src/SD2_coursework_IIT/Ticket.java
@@ -0,0 +1,56 @@
+package SD2_coursework_IIT;
+
+public class Ticket {
+
+	private String row;
+	private int seat;
+	private int price;
+	private Person person;
+
+	public Ticket(String row, int seat, int price, Person person) {
+		this.row = row;
+		this.seat = seat;
+		this.price = price;
+		this.person = person;
+	}
+	
+	public String getRow() {
+		return row;
+	}
+	
+	public void setRow(String row) {
+		this.row = row;
+	}
+	
+	public int getSeat() {
+		return seat;
+	}
+	
+	public void setSeat(int seat) {
+		this.seat = seat;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public Person getPerson() {
+		return person;
+	}
+	
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+	public void print_ticketInfo() {
+		System.out.println("Row :"+row);
+		System.out.println("Seat :"+seat);
+		System.out.println("Price :"+price);
+		person.print_personInfo();
+
+	}
+}
